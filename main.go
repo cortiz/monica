3b0a5458ba4e84fd2465b8a6a405d075fcecad49
Carlos Ortiz <cortiz@jmpeax.com>
2024-06-14 18:56:26 -0600

    Basic Runner of a single mon file

--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import (
 	"flag"
 	"github.com/rs/zerolog"
 	"jmpeax.com/sec/monica/internal/logging"
+	"jmpeax.com/sec/monica/pkg/runner"
+	"os"
 )
 
 var (
-	verbosity = flag.Int("v", -1, "verbosity level")
+	verbosity  = flag.Int("v", -1, "verbosity level")
+	file       = flag.String("f", "", "Mon File to run")
+	headerOnly = flag.Bool("h", false, "Output headers only")
 )
 
 func main() {
@@ -21,4 +25,19 @@ func main() {
 		zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	}
 	logging.Log.Info().Msg("Welcome to Monica!")
+	if *file == "" {
+		logging.Log.Error().Msg("No Mon File provided")
+		return
+	}
+	monFile, err := os.Stat(*file)
+	if err != nil {
+		logging.Log.Error().Msgf("Mon File not found: %s", *file)
+		return
+	}
+	if !monFile.Mode().IsRegular() {
+		logging.Log.Error().Msgf("Mon File is not a regular file: %s", *file)
+		return
+	}
+	logging.Log.Info().Msgf("Running Mon File: %s", *file)
+	runner.RunSingleFile(*file, &runner.Opts{HeaderOnly: *headerOnly})
 }
